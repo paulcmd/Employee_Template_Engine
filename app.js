8b4fa40ee387de77206e44bfe8b07d70ad309b21
paulcmd <choxmail.pm@gmail.com>
2020-05-07 16:10:52 -0700

    Employee classes functioning

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
+
+const express = require("express");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​const app = express();
+
+const app = express();
 const PORT = 3000;
 
+// Empty employee array
 const employeeArray = [];
 
-function pickEmployee() {
+//___________generate an employee from the list______________________________________
+function generateEmployee() {
     inquirer.prompt([
         {
             type: "list",
             name: "employeeRole",
             message: "Pick a team member to add : ",
             choices: [
-                "Manager", "Engineer", "Intern", "Done"
+                "Manager", "Engineer", "Intern", "Exit"
             ]
         }
     ]).then(answers => {
         if (answers.employeeRole === "Manager") {
-            generateIntern();
-        } else if (answers.employeeRole === "Engineer") {
             generateManager();
+
+        } else if (answers.employeeRole === "Engineer") {
+            generateEngineer();
+
         } else if (answers.employeeRole === "Intern") {
             generateIntern();
+
         } else {
             let html = render(employeeArray);
 
             fs.writeFile(outputPath, html, (err) => {
                 if (err) throw err;
-                console.log("The file was written");
+                console.log("The file has been created!");
             });
         }
     });
@@ -47,17 +53,17 @@ function generateIntern() {
     inquirer.prompt([
         {
             type: "input",
-            message: "What is the Intern's name?",
+            message: "What is the employee's name?",
             name: "name"
         },
         {
             type: "input",
-            message: "What is the employee id?",
+            message: "What is the employee's id?",
             name: "id"
         },
         {
             type: "input",
-            message: "What is the employee email?",
+            message: "What is the employee's email?",
             name: "email"
         },
         {
@@ -67,8 +73,9 @@ function generateIntern() {
         }
     ]).then(answers => {
         let employee = new Intern(answers.name, answers.id, answers.email, answers.school);
+        console.log(employee);
         employeeArray.push(employee);
-        employeeType();
+        generateEmployee();
     })
 }
 
@@ -76,28 +83,29 @@ function generateManager() {
     inquirer.prompt([
         {
             type: "input",
-            message: "What is the employee name?",
+            message: "What is the employee's name?",
             name: "name"
         },
         {
             type: "input",
-            message: "What is the employee id?",
+            message: "What is the employee's id?",
             name: "id"
         },
         {
             type: "input",
-            message: "What is the employee email?",
+            message: "What is the employee's email?",
             name: "email"
         },
         {
             type: "input",
             message: "What is the employee's office number?",
-            name: "office"
+            name: "officeNumber"
         }
     ]).then(answers => {
-        let employee = new Manager(answers.name, answers.id, answers.email, answers.office);
+        let employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        console.log(employee);
         employeeArray.push(employee);
-        pickEmployee();
+        generateEmployee();
     })
 }
 
@@ -105,17 +113,17 @@ function generateEngineer() {
     inquirer.prompt([
         {
             type: "input",
-            message: "What is the employee name?",
+            message: "What is the employee's name?",
             name: "name"
         },
         {
             type: "input",
-            message: "What is the employee id?",
+            message: "What is the employee's id?",
             name: "id"
         },
         {
             type: "input",
-            message: "What is the employee email?",
+            message: "What is the employee's email?",
             name: "email"
         },
         {
@@ -125,33 +133,10 @@ function generateEngineer() {
         }
     ]).then(answers => {
         let employee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        console.log(employee);
         employeeArray.push(employee);
-        pickEmployee();
+        generateEmployee();
     })
 }
 
-pickEmployee();
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+generateEmployee();
